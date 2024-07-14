2161847076ba8d24387c57050aee55ea15460f2e
mejia-dev <aaron.mejia8484@gmail.com>
2024-07-14 16:45:29 -0700

    Add injector.js to add inject.js to page

---
+++ src/scripts/injector.js
@@ -0,0 +1,15 @@
+(async () => {
+  const injectScript = await chrome.runtime.getURL("scripts/inject.js");
+  const script = document.createElement("script");
+  script.setAttribute("src", injectScript);
+  script.setAttribute("type", "text/javascript");
+  script.onload = function () {
+    this.remove();
+    chrome.storage.local.get(["uploadedAudio"], function (result) {
+      if (result.uploadedAudio) {
+        document.dispatchEvent(new CustomEvent("FBMNSC-Initialize", { detail: result.uploadedAudio }));
+      }
+    });
+  }
+  document.head.appendChild(script);
+})();
